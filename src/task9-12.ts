34998c72eabd318d2a9819e589ee7ed1e2c0f56a
masanori0117 <masanori.takahashi.0117@gmail.com>
2024-12-16 00:56:52 +0900

    Modified task9-12.ts

--- src/task9-12.ts
+++ src/task9-12.ts
@@ -1,9 +1,9 @@
 // ### 9. for文と条件式の組み合わせ3
 console.log("20 ~ 50までの数字の中で2で割ったら奇数となる数字のみを出力してください")
 
-for (let num9: number = 20; num9 < 51; num9++) {
-    if ((num9 / 2) % 1 === 0 && (num9 / 2) % 2 !== 0) {
-        console.log(num9)
+for (let num: number = 20; num < 51; num++) {
+    if ((num / 2) % 1 === 0 && (num / 2) % 2 !== 0) {
+        console.log(num)
     }
 }
 
@@ -12,8 +12,12 @@ for (let num9: number = 20; num9 < 51; num9++) {
 console.log("20 ~ 50までの数字の中で2で割ったら奇数となる数字の個数を出力してください")
 
 let count: number = 0;
-for (let num10: number = 20; num10 < 51; num10++) {
-    if ((num10 / 2) % 1 === 0 && (num10 / 2) % 2 !== 0) {
+for (let num: number = 20; num < 51; num++) {
+    let div = num / 2
+    if (div % 1 !==0){
+        continue;
+    }
+    if (div % 2 !== 0) {
         count++;
     }
 }
@@ -24,8 +28,8 @@ console.log(count);
 console.log("1000未満の「3と7の倍数」は何個あるか　個数を出力してください")
 
 count = 0;
-for (let num11: number = 0; num11 < 1000; num11++) {
-    if (num11 % 3 === 0 || num11 % 7 === 0) {
+for (let num: number = 0; num < 1000; num++) {
+    if (num % 3 === 0 || num % 7 === 0) {
         count++;
     }
 }
@@ -35,11 +39,11 @@ console.log(count);
 console.log("1000未満の「3と7の倍数」の5番目に大きい数を出力してください")
 
 count = 0;
-for (let num12: number = 999; num12 > 0; num12--) {
-    if (num12 % 3 === 0 || num12 % 7 === 0) {
+for (let num: number = 999; num > 0; num--) {
+    if (num % 3 === 0 || num % 7 === 0) {
         count++;
         if (count === 5) {
-            console.log(num12)
+            console.log(num)
             break;
         }
     }

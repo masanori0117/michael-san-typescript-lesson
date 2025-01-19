b72b1bd7225aa9c91e6e66169d760a224644bd27
masanori0117 <masanori.takahashi.0117@gmail.com>
2025-01-20 00:44:21 +0900

    Modified task23-25.ts

--- src/task23-25.ts
+++ src/task23-25.ts
@@ -1,17 +1,17 @@
 // ### 23. `map` を使った配列の操作
 console.log("1. 配列 `[2, 4, 6, 8, 10]` の各要素を半分にした新しい配列を作成してください。")
-let numbers = [2, 4, 6, 8, 10];
-const halfNumbers = numbers.map(number => number / 2)
+let numbers: number[] = [2, 4, 6, 8, 10];
+const halfNumbers: number[] = numbers.map(number => number / 2)
 console.log(halfNumbers)
 
 console.log("2. 文字列配列 `['a', 'b', 'c', 'd', 'e']` の各要素を `'_'` で囲んだ新しい配列を作成してください（例：`'_a_'`）。")
-const letters = ['a', 'b', 'c', 'd', 'e'];
-const newLetters = letters.map(letter => `_${letter}_`);
+const letters: string[] = ['a', 'b', 'c', 'd', 'e'];
+const newLetters: string[] = letters.map(letter => `_${letter}_`);
 console.log(newLetters)
 
 console.log("3. 数字の配列 `[1, 2, 3, 4, 5]` を使って、各要素を文字列に変換した新しい配列を作成してください。")
 numbers = [1, 2, 3, 4, 5];
-const numberssToStrings = numbers.map(number => number.toString());
+const numberssToStrings: string[] = numbers.map(number => number.toString());
 console.log(numberssToStrings)
 
 // ### 24. `forEach` を使った配列の操作
@@ -26,27 +26,27 @@ for (let num = 0; num < numbers.length; num++) {
 }
 
 console.log("3. 文字列配列 `['cat', 'dog', 'bird']` の各要素の先頭に「動物:」を追加して表示してください。")
-const animals = ['cat', 'dog', 'bird'];
+const animals: string[] = ['cat', 'dog', 'bird'];
 animals.forEach(animal => console.log(`動物: ${animal}`))
 
 // ### 25. 配列の高度な操作
 console.log("1. 配列 `[1, 2, 3, 4, 5, 6]` の要素を合計してください。")
 numbers = [1, 2, 3, 4, 5, 6]
-let totalNumber = numbers.reduce((total, number) => total + number, 0);
+let totalNumber: number = numbers.reduce((total, number) => total + number, 0);
 console.log(totalNumber);
 
 console.log("2. 配列 `[10, 20, 30, 40, 50]` の平均値を計算してください。")
 numbers = [10, 20, 30, 40, 50]
 totalNumber = numbers.reduce((total, number) => total + number, 0)
-const averageNumber = totalNumber / numbers.length;
+const averageNumber: number = totalNumber / numbers.length;
 console.log(averageNumber);
 
 console.log("3. 数字が重複している配列 `[1, 2, 2, 3, 4, 4, 5]` から重複を削除して新しい配列を作成してください。")
 numbers = [1, 2, 2, 3, 4, 4, 5]
-const uniqueNumbers = [...new Set(numbers)];
+const uniqueNumbers: number[] = [...new Set(numbers)];
 console.log(uniqueNumbers);
 
 console.log("4. 配列 `[5, 3, 8, 1, 2, 7]` を昇順にソートしてください。")
 numbers = [5, 3, 8, 1, 2, 7]
-const sortedNumbers = numbers.sort((a, b) => a - b);
+const sortedNumbers: number[] = numbers.sort((a, b) => a - b);
 console.log(sortedNumbers);

0924f4573fb43fa126565190662c5d2488fd1fbe
masanori0117 <masanori.takahashi.0117@gmail.com>
2025-02-12 11:11:25 +0900

    Modified task30-33.ts

--- src/task30-33.ts
+++ src/task30-33.ts
@@ -1,6 +1,5 @@
 console.log("### **30. 基本的な関数**");
 console.log("1. 数値を受け取り、その数値の2乗を返す関数を作成してください。");
-
 function square(num: number): number {
     return num * num;
 }
@@ -24,32 +23,31 @@ console.log(sumNumber(11,20));
 
 console.log("### **31. デフォルト引数**");
 console.log('1. 名前を引数として受け取り、デフォルト値を `"Guest"` に設定し、`Welcome, [名前]!` と出力する関数を作成してください。');
-function welcome_name(name : string = "Guest"): string {
+function welcomeName(name : string = "Guest"): string {
     return `Welcome, ${name}!`;
 }
-console.log(welcome_name());
-console.log(welcome_name("山田"));
+console.log(welcomeName());
+console.log(welcomeName("山田"));
 
 console.log("2. 2つの数値を引数として受け取り、2番目の引数のデフォルト値を10に設定して、それらを掛け算する関数を作成してください。")
-function multiplyNumber(num1: number, num2: number) {
+function multiplyNumber(num1: number, num2: number = 10) {
     return num1 * num2
 }
 console.log(multiplyNumber(2,4));
-console.log(multiplyNumber(10,10));
+console.log(multiplyNumber(10));
 
 console.log("### **32. オプショナル引数**")
 console.log("1. 2つの数値を引数として受け取り、2番目の引数が指定されている場合はそれらを掛け算し、指定されていない場合は1番目の数値をそのまま返す関数を作成してください。")
-function multiplyOrResturn(num1: number, num2?: number): number {
+function multiplyOrReturn(num1: number, num2?: number): number {
     return num2 !== undefined ? num1 * num2 : num1;
 }
-console.log(multiplyOrResturn(2,4));
-console.log(multiplyOrResturn(2));
+console.log(multiplyOrReturn(2,4));
+console.log(multiplyOrReturn(2));
 
 console.log("2. 名前を引数として受け取り、オプショナル引数で年齢を受け取り、`[名前] is [年齢] years old.` または `[名前] is ageless.` を出力する関数を作成してください。")
 function displayYearAndName(name: string, age?: number): string {
-    return age !== undefined ? `${name} is ${age} years old.` : `${name} is ageless`
+    return age !== undefined ? `${name} is ${age} years old.` : `${name} is ageless.`
 }
-
 console.log(displayYearAndName("山田", 15));
 console.log(displayYearAndName("田中"));
 
